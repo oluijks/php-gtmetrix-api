4be72e42583b501b7b8c9c65b57902649d39c508
Olaf Luijks <oluijks@gmail.com>
2013-09-28 17:57:46 +0200

    Added some basic PHPUnit tests

---
+++ tests/GTmetrixApiTest.php
@@ -0,0 +1,61 @@
+<?php namespace GTmetrix;
+
+/**
+ *
+ *
+ * @version 0.0.1 
+ * @author  Olaf Luijks <oluijks@gmail.com>
+ */
+
+use GTmetrix\Api;
+
+class GTmetrixApiTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var
+     */
+    protected $_api;
+
+    /**
+     *
+     */
+    public function setUp()
+    {
+        $this->_api = new Api();
+    }
+
+    /**
+     *
+     * @expectedException InvalidArgumentException
+     */
+    public function testEmptyUsernameThrowsInvalidArgumentException()
+    {
+        $this->_api->setUsername('');
+    }
+
+    /**
+     *
+     * @expectedException InvalidArgumentException
+     */
+    public function testInvalidUsernameThrowsInvalidArgumentException()
+    {
+        $this->_api->setUsername('invalid-email-address');
+    }
+
+    /**
+     *
+     * @expectedException InvalidArgumentException
+     */
+    public function testEmptyApiKeyThrowsInvalidArgumentException()
+    {
+        $this->_api->setApiKey('');
+    }
+
+    /**
+     *
+     */
+    public function tearDown()
+    {
+        $this->_api = null;
+    }
+}
